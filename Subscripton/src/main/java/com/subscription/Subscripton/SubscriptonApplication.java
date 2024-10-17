0736730bcbf7d8d25ac637aaf99ea7bceeeda642
Harry3795 <pradeepkumar20495@gmail.com>
2024-10-17 15:51:36 +0530

    new subscription

---
+++ Subscripton/src/main/java/com/subscription/Subscripton/SubscriptonApplication.java
@@ -0,0 +1,13 @@
+package com.subscription.Subscripton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptonApplication.class, args);
+	}
+
+}
